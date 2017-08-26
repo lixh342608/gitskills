5737d48103603b5c6f0a28b5ac1a244bfe874fb9
lixh342608 <li_xinghuay@163.com>
2017-08-26 12:13:29 +0800

    优化

--- gitskills/lishitest/time_out.py
+++ gitskills/lishitest/time_out.py
@@ -4,6 +4,9 @@ Created on 2016年3月1日
 
 @author: admin
 '''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException,WebDriverException,ElementNotVisibleException
 """关于本类get_ele函数：元素定位方式错误、定位出错或超时给出错误信息返回0，
@@ -50,16 +53,65 @@ class waittime:
         except Exception:
             print("程序出现错误!信息：%s" % value)
             return 0
-        
-        
+    def change_fashion(self,fashion):
+        if fashion.lower()=="id":#ID方法定位元素
+            return By.ID
+            
+        elif fashion.lower()=="name":#NAME方法定位元素
+            return By.NAME
+        elif fashion.lower()=="xpath":#XPATH方法定位元素
+            return By.XPATH
+        elif fashion.lower()=="class":#class_name方法定位元素
+            return By.CLASS_NAME
+        elif fashion.lower()=="css":#css_selector方法定位元素
+            return By.CSS_SELECTOR
+        elif fashion.lower()=="tag":#tag_name方法定位元素
+            return By.TAG_NAME
+        elif fashion.lower()=="link":#link_text方法定位元素
+            return By.LINK_TEXT
+        elif fashion.lower()=="partial":#partial_link_text方法定位元素(匹配链接的部分文字)
+            return By.PARTIAL_LINK_TEXT
+        else:
+            print("定位方式无效！")
+            return 0
+    #有效时间内控件是可见，可见返回元素，不可见提示超时信息
+    def visibility(self,fashion,value):
+        try:
+            BY=self.change_fashion(fashion)
+            ele=self.wait.until(EC.visibility_of(self.driver.find_element(by=BY,value=value)))
+            return ele
+        except TimeoutException:
+            print('定位元素超时',value)
+            return None
+    #有效时间内定位到控件，返回元素，超时提示超时信息    
+    def presence(self,fashion,value):
+        try:
+            BY=self.change_fashion(fashion)
+            ele=self.wait.until(EC.presence_of_element_located((BY,value)))
+            return ele
+        except TimeoutException:
+            print('定位元素超时',value)
+            return None
+    #有效时间内定位到元素并且为可点击状态，并返回元素，否则提示超时信息
+    def clickable(self,fashion,value):
+        try:
+            BY=self.change_fashion(fashion)
+            ele=self.wait.until(EC.element_to_be_clickable((BY,value)))
+            return ele
+        except TimeoutException:
+            print('定位元素超时',value)
+            return None
+
 if __name__=="__main__":
-    from selenium import webdriver
+
     
     driver=webdriver.Chrome('C:/chromedriver')
-    driver.get('https://jr.yatang.cn/NewLogin/index/referer/')
+    driver.get('https://www.baidu.com/')
     driver.maximize_window()
-    driver.switch_to.frame(0)
-    ele=driver.find_element_by_xpath('//*[@id="js-username"]').send_keys('月光宝盒')
+    #driver.switch_to.frame(0)
+    wait=WebDriverWait(driver,10)
+    ele=wait.until(EC.presence_of_element_located((By.ID,'aw')))
+    ele.send_keys('月光宝盒')
     print(ele)
     '''wait=waittime(driver,30)
     wait.get_ele('xpath','//*[@id="js-username"]').send_keys('月光宝盒')
--- gitskills/lishitest/tkset.py
+++ gitskills/lishitest/tkset.py
@@ -5,8 +5,6 @@ Created on 2017年6月16日
 @author: Administrator
 '''
 from selenium import webdriver
-from time import sleep
-from selenium.common.exceptions import NoSuchElementException
 import pickle,threading
 from time_out import waittime
 from tkinter import *
@@ -26,7 +24,7 @@ def loadcol():
 
 #使用requests实现查询第一页期限一个月，待投金额大于X，利率最高的标
 def reque_num(main_num):
-    sleep(1.5)
+    time.sleep(1.5)
     url='https://jr.yatang.cn/Financial/getAssetList'
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36'}
     data={}
@@ -37,8 +35,7 @@ def reque_num(main_num):
     data['page_href']='/Financial/getAssetList?&p=1'
     resq=requests.post(url,data,headers)
     t_text=json.loads(resq.text)['list']
-    
-    t_text=[i for i in t_text if i['showday']=='1个月' and i['remain'] > main_num]# and i['borrow_type'] != '10']
+    t_text=[i for i in t_text if i['showday']=='1个月' and i['remain'] > main_num and float(i['apr']) >= 5.0]# and i['borrow_type'] != '10']
     #for i in t_text:
         #print(i)
     apr=0
@@ -51,11 +48,13 @@ def reque_num(main_num):
     return apr_num   
 
 class q_mon(threading.Thread):
-    def __init__(self,eve,msg_var,commit_bt):
+    def __init__(self,eve,msg_var,commit_bt,miao_bt,stop_bt):
         super(q_mon,self).__init__()
         self.eve=eve
         self.msg_var=msg_var
         self.commit_bt=commit_bt
+        self.miao_bt=miao_bt
+        self.stop_bt=stop_bt
         self.nopar=[]
         self.yuecol=loadcol()
         self.username=self.yuecol[0]#用户名
@@ -63,7 +62,7 @@ class q_mon(threading.Thread):
         self.paypwd=self.yuecol[2]#支付密码
         self.par_num=int(self.yuecol[3])#红包倍数
         self.parsex_list=self.yuecol[4].split(',')#红包面值'''
-        #self.driver=webdriver.PhantomJS()
+
         self.driver=webdriver.Chrome('C:/chromedriver')
         self.driver.get('https://jr.yatang.cn/Financial/asset')
         
@@ -72,40 +71,22 @@ class q_mon(threading.Thread):
     def run(self):
         self.eve.wait()
         self.q_mont()
-    def persi_ele(self,by_vale,byse='xpath',col=1000):
-        y_col=0
-        while y_col<=col:
-            ele=self.wait.get_ele(byse,by_vale)
-            if ele==0:
-                y_col+=1
-                continue
-            else:
-                if ele.is_displayed():
-                    return ele
-                else:
-                    print("控件[%s]不可见！" % by_vale)
-                    y_col+=1
-                    continue
-        else:
-            return None
          
     def login(self):
         self.driver.get('https://jr.yatang.cn/NewLogin/index/referer/')
         self.driver.switch_to.frame(0)
-        self.persi_ele('//*[@id="js-username"]').send_keys(self.username)
+        self.wait.visibility('xpath','//*[@id="js-username"]').send_keys(self.username)
         
-        self.persi_ele('//*[@id="js-password"]').send_keys(self.pwd)
+        self.wait.visibility('xpath','//*[@id="js-password"]').send_keys(self.pwd)
         
         
-        self.persi_ele('//*[@id="js-login"]').click()
-        sleep(2)
+        self.wait.clickable('xpath','//*[@id="js-login"]').click()
+        time.sleep(2)
         login_ele=self.wait.get_ele('xpath','//*[@id="top"]/div[1]/div/div[2]/a[2]')
         if login_ele and login_ele.text=='免费注册':
             self.login()
         else:
             self.msg_var.set('登陆成功')
-
-            
     def q_mont(self):
 
         if not self.eve.is_set():
@@ -123,14 +104,14 @@ class q_mon(threading.Thread):
         #循环直到找到为止
         apr_num=0
         while int(apr_num)==0:
-            now=time.strftime('%Y-%m-%d',time.localtime(time.time()))
-            if now!="2017-07-12":
+            '''now=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+            if now!="2017-08-11":
                 try:
                     sys.exit(0)
                 except:
                     tkinter.messagebox.showinfo('报告', '程序已失效！')
                 finally:
-                    self.eve.clear()
+                    self.eve.clear()'''
                 
             if not self.eve.is_set():
                 self.msg_var.set("抢标已暂停")
@@ -157,7 +138,7 @@ class q_mon(threading.Thread):
         self.msg_var.set("抢标成功，\n准备投标中...")
         url='https://jr.yatang.cn/Invest/ViewBorrow/ibid/%s' % apr_num
         self.driver.get(url)
-        mynum=self.persi_ele('//*[@id="amountt"]',col=0)
+        mynum=self.wait.visibility('xpath','//*[@id="amountt"]')
         if mynum:
             mynum.send_keys(main_num) 
             self.msg_var.set("选择红包中...")
@@ -165,11 +146,11 @@ class q_mon(threading.Thread):
             self.msg_var.set("标被人抢先一步！")
             self.q_mont()
         #查找红包选框
-        hbxs_ele=self.persi_ele('hbje_xs','class',3)
+        hbxs_ele=self.wait.visibility('class','hbje_xs')
         if hbxs_ele:
             hbxs_ele.click()
             #找到红包盒子
-            ele_box=self.persi_ele('hb_xl_box','class') 
+            #ele_box=self.wait.visibility('class','hb_xl_box')
             try:
                 #找到盒子中所有红包
                 hb_list=self.driver.find_elements_by_class_name('hb_check_list')
@@ -212,12 +193,12 @@ class q_mon(threading.Thread):
         
 
     def biaochu(self):
-        self.persi_ele('//*[@id="incheck"]').click()
-        pay_ele=self.persi_ele('//*[@id="ppay"]',col=10)
+        self.wait.clickable('xpath','//*[@id="incheck"]').click()
+        pay_ele=self.wait.visibility('xpath','//*[@id="ppay"]')
         if pay_ele!=None:
         
             pay_ele.send_keys(self.paypwd)
-            self.persi_ele('//*[@id="button"]').click()
+            self.wait.clickable('xpath','//*[@id="button"]').click()
             ti_mm=15
             while ti_mm>=0:
                 self.msg_var.set('投标已进入队列，\n等待15秒（%d）' % ti_mm)
@@ -226,7 +207,169 @@ class q_mon(threading.Thread):
             
     def chnge_bt(self):
         self.commit_bt['state']=NORMAL
-        self.commit_bt["text"]="继续投资"        
+        self.stop_bt['state']=DISABLED
+        self.miao_bt['state']=NORMAL
+
+
+class lishitest(threading.Thread):
+    def __init__(self,eve,msg_var,commit_bt,miao_bt,stop_bt,miao_num=None):
+        super(lishitest, self).__init__()
+        self.eve=eve
+        self.msg_var=msg_var
+        self.commit_bt=commit_bt
+        self.miao_bt=miao_bt
+        self.stop_bt=stop_bt
+        self.miao_num=miao_num
+        self.yuecol=loadcol()
+        self.username=self.yuecol[0]#用户名
+        self.pwd=self.yuecol[1]#用户密码
+        self.paypwd=self.yuecol[2]#支付密码
+        self.driver = webdriver.Chrome('C:/chromedriver')
+        self.driver.get('https://jr.yatang.cn/Financial/welfare')
+
+        self.driver.maximize_window()
+        self.wait = waittime(self.driver, 20)
+
+    def run(self):
+        self.eve.wait()
+        self.miao_main()
+    def miao_main(self):
+        pac = self.timeset()
+        if pac==0:
+            self.giter()
+        else:
+            self.driver.quit()
+
+    def lishinum(self):
+        pr_ele = self.wait.presence('class', 'wf_move_box')
+        if not pr_ele:
+            return '0'
+        ele_list = pr_ele.find_elements_by_class_name('wf_list_box')
+        ele_list = filter(lambda x: x.get_attribute('iborrowid') != '0', ele_list)
+        lishinumer = 0
+        ele_praser = 0
+        for i in ele_list:
+            ele = i.find_element_by_class_name('wf_xq_lv')
+            ele_pra = int(re.sub('\W', '', ele.text))
+            if ele_pra > ele_praser:
+                ele_praser = ele_pra
+                lishinumer = i.get_attribute('iborrowid')
+        return lishinumer
+
+    def set_xpath(self, lishinumer):
+        self.numer_xpath = '//*[@id="amountt_%s"]' % lishinumer
+        self.check_xpath = '//*[@id="incheck_%s"]' % lishinumer
+        self.ppay_xpath = '//*[@id="ppay_%s"]' % lishinumer
+        self.commit_xpath = '//*[@id="button_%s"]' % lishinumer
+        self.withdra_class = 'aj_withdrawalCash_%s' % lishinumer
+        self.time_class = 'lefttime_%s' % lishinumer
+
+    def login(self):
+        self.driver.get('https://jr.yatang.cn/NewLogin/index/referer/')
+        self.driver.switch_to.frame(0)
+        self.wait.visibility('xpath', '//*[@id="js-username"]').send_keys(self.username)
+
+        self.wait.visibility('xpath', '//*[@id="js-password"]').send_keys(self.pwd)
+
+        self.wait.clickable('xpath', '//*[@id="js-login"]').click()
+
+        time.sleep(2)
+        login_ele = self.wait.get_ele('xpath', '//*[@id="top"]/div[1]/div/div[2]/a[2]')
+
+        if login_ele.text == '免费注册':
+            self.login(username, pwd)
+
+    def wrrtenum(self, numer):
+        if numer == None:
+            dra_text = self.wait.presence('class', self.withdra_class).text
+
+            numer = int(float(dra_text.strip('￥').replace(',', '')))
+
+        self.wait.visibility('xpath', self.numer_xpath).send_keys(numer)
+
+    def giter(self):
+        self.wrrtenum(self.miao_num)
+        ele = self.wait.clickable('xpath', self.check_xpath)
+        ele.click()
+        self.miaochu()
+
+    def miaochu(self, pay_pwd):
+        pay_ele = self.wait.visibility('xpath', self.ppay_xpath)
+        pay_ele.send_keys(self.paypwd)
+        commit_ele = self.wait.clickable('xpath', self.commit_xpath)
+        commit_ele.click()
+
+        check_ele = self.wait.visibility('xpath','/html/body/div[5]/div/div/div/div[1]/div[1]/b')
+        print(check_ele.text)
+
+    def timeset(self):
+        lishinumer = self.lishinum()
+        if lishinumer == '0':
+            self.msg_var.set('没有可投秒标！')
+            tkinter.messagebox.showinfo('报告', '没有可投秒标！')
+            try:
+                sys.exit(0)
+            except:
+                self.msg_var.set('程序准备退出！')
+                tkinter.messagebox.showinfo('报告', '程序准备退出！')
+            finally:
+                self.chnge_bt()
+                self.msg_var.set('设置投资信息...')
+                return 2
+        self.set_xpath(lishinumer)
+
+        while True:
+            if not self.eve.is_set():
+                self.msg_var.set("抢标已暂停")
+                self.chnge_bt()
+                self.driver.quit()
+                self.eve.wait()
+            '''now=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+            if now!="2017-08-11":
+                try:
+                    sys.exit(0)
+                except:
+                    tkinter.messagebox.showinfo('报告', '程序已失效！')
+                finally:
+                    return 1'''
+            self.driver.refresh()
+            login_ele = self.wait.get_ele('xpath', '//*[@id="top"]/div[1]/div/div[2]/a[2]')
+
+            if login_ele.text == '免费注册':
+                self.msg_var.set('正在登录...')
+                self.login()
+                self.msg_var.set('登录成功！')
+                self.driver.get('https://jr.yatang.cn/Financial/welfare')
+            ti_text = self.wait.get_ele('class', self.time_class).text
+            ti_list = ti_text.split(':')
+            ti_sum = int(ti_list[0]) * 3600 + int(ti_list[1]) * 60 + int(ti_list[2])
+            # ti_sum=int(ti_list[2])
+            self.msg_var.set('距离投秒时间还有%d秒，等待刷新...' % ti_sum)
+            if ti_sum > 600:
+                sleep(300)
+                continue
+            elif ti_sum > 300:
+                sleep(180)
+                continue
+            elif ti_sum > 200:
+                sleep(90)
+                continue
+            elif ti_sum > 50:
+                sleep(12)
+                continue
+            elif ti_sum > 10:
+                sleep(8)
+                continue
+            elif ti_sum > 5:
+                sleep(ti_sum - 2)
+                continue
+            else:
+                break
+        return 0
+    def chnge_bt(self):
+        self.commit_bt['state']=NORMAL
+        self.stop_bt['state']=DISABLED
+        self.miao_bt['state']=NORMAL
 class yuebiao_gui:
     def __init__(self):
         self.plat='Z6NRQGUFOYCRJGFWindows-7-6.1.7601-SP1'
@@ -236,7 +379,7 @@ class yuebiao_gui:
         self.root.title('你值得拥有')
         width_n=self.root.winfo_screenwidth()/2-200
         height_n=self.root.winfo_screenheight()/2-200
-        self.root.geometry('400x400+%d+%d' % (int(width_n),int(height_n)))
+        self.root.geometry('400x500+%d+%d' % (int(width_n),int(height_n)))
 
 
     
@@ -247,9 +390,10 @@ class yuebiao_gui:
         if col_list==0:
             col_list=["","","","",""]
 
-        def click_on():
-            if platform.node()+platform.platform()!=self.plat:self.root.destroy()
-            
+        def yue_click_on():
+            if platform.node()+platform.platform()!=self.plat:
+                tkinter.messagebox.showerror('运行出错','此程序不可在此计算机上运行！')
+                return
             
             username=username_var.get()
             pwd=pwd_var.get()
@@ -259,44 +403,71 @@ class yuebiao_gui:
             if username and pwd and paypwd and pranum and hbset:
                 stop_bt['state']=NORMAL
                 commit_bt['state']=DISABLED
+                miao_bt['state'] = DISABLED
                 yue_col=[username,pwd,paypwd,pranum,hbset]
                 writcol(yue_col)
                 pack_var.set('正在初始化...')
                 self.eve=threading.Event()
-                self.tasker=q_mon(self.eve,pack_var,commit_bt)
+                self.tasker=q_mon(self.eve,pack_var,commit_bt,miao_bt,stop_bt)
                 self.tasker.setDaemon(True)
                 self.tasker.start()
-                sleep(2)
+                time.sleep(2)
                 self.eve.set()
             else:
-                tkinter.messagebox.askokcancel("提示","所有项不可为空!")
-     
+                tkinter.messagebox.askokcancel("提示","缺少有用设置信息，请检查！")
+        def miao_click_on():
+            if platform.node()+platform.platform()!=self.plat:
+                tkinter.messagebox.showerror('运行出错','此程序不可在此计算机上运行！')
+                return
+            username=username_var.get()
+            pwd=pwd_var.get()
+            paypwd=paypwd_var.get()
+            miao_num=miao_var.get()
+            if username and pwd and paypwd:
+                stop_bt['state']=NORMAL
+                commit_bt['state']=DISABLED
+                miao_bt['state'] = DISABLED
+                yue_col=[username,pwd,paypwd,'','']
+                writcol(yue_col)
+                if not miao_num:
+                    miao_num=None
+                pack_var.set('正在初始化...')
+                self.eve=threading.Event()
+                self.tasker=lishitest(self.eve,pack_var,commit_bt,miao_bt,stop_bt,miao_num)
+                self.tasker.setDaemon(True)
+                self.tasker.start()
+                time.sleep(2)
+                self.eve.set()
+
+
         def stop_tb():
             self.eve.clear()
+            stop_bt['state'] = DISABLED
             commit_bt['state']=NORMAL
-                    
+            miao_bt['state'] = NORMAL
         
         Label(self.root,width=7).grid(row=0,column=0)
         Label(self.root).grid(row=1,column=0)
         Label(self.root).grid(row=3,column=0)
         Label(self.root).grid(row=5,column=0)
         Label(self.root).grid(row=7,column=0)
-        Label(self.root,text='红包面值可设置多个，使用英文逗号（,）隔开。',bd=5).grid(row=9,column=1,columnspan=3)
-        Label(self.root).grid(row=11,column=0)
+        Label(self.root,text='红包面值可设置多个，使用英文逗号（,）隔开。\n红包设置只在投资月标时生效！',bd=5).grid(row=9,column=1,columnspan=3)
+        Label(self.root,text='投秒资金只在投资秒标时生效！').grid(row=11,column=1,columnspan=3)
         Label(self.root,text="雅堂帐户:",bd=5).grid(row=0,column=1)
         Label(self.root,text="帐户密码:",bd=5).grid(row=2,column=1)
         Label(self.root,text="交易密码:",bd=5).grid(row=4,column=1)
         Label(self.root,text="使用红包倍数:",bd=5).grid(row=6,column=1)
-        Label(self.root,text="当前动作:",bd=5,font="华康少女字体,16").grid(row=12,column=1,rowspan=2)
+        Label(self.root,text="当前动作:",bd=5,font="华康少女字体,16").grid(row=14,column=1,rowspan=2)
         Label(self.root,text="使用红包面值:",bd=5).grid(row=8,column=1)
+        Label(self.root, text="投秒资金:", bd=5).grid(row=10, column=1)
         
         hb_var=StringVar()
-        Entry(self.root,textvariable=hb_var,bd=5).grid(row=8,column=2)
+        Entry(self.root,textvariable=hb_var,bd=5).grid(row=8,column=2,columnspan=2)
         hb_var.set(col_list[4])
         
         
         pack_var=StringVar()
-        Label(self.root,bd=5,textvariable=pack_var,font=16,fg='green').grid(row=12,column=2,rowspan=2,columnspan=2)
+        Label(self.root,bd=5,textvariable=pack_var,font=16,fg='green').grid(row=14,column=2,rowspan=2,columnspan=2)
         pack_var.set('设置投资信息...')
         
         username_var=StringVar()
@@ -313,13 +484,19 @@ class yuebiao_gui:
     
         pranum_var=StringVar()
         Entry(self.root,textvariable=pranum_var,bd=5).grid(row=6,column=2,columnspan=2)
-        pranum_var.set(col_list[3])    
+        pranum_var.set(col_list[3])
+
+        miao_var=StringVar()
+        Entry(self.root,textvariable=miao_var,bd=5).grid(row=10,column=2,columnspan=2)
+        miao_var.set('')
         
         
-        commit_bt=Button(self.root,text="开始抢标",command=click_on,bg="green",width=15,fg="blue",bd=5)
-        commit_bt.grid(row=10,column=2,columnspan=2)
-        stop_bt=Button(self.root,text="停止抢标",command=stop_tb,bg="red",width=15,fg="blue",bd=5,state=DISABLED)
-        stop_bt.grid(row=10,column=1)            
+        commit_bt=Button(self.root,text="我要抢标",command=yue_click_on,bg="green",width=10,fg="blue",bd=5)
+        commit_bt.grid(row=12,column=2)
+        stop_bt=Button(self.root,text="停止抢标",command=stop_tb,bg="red",width=10,fg="blue",bd=5,state=DISABLED)
+        stop_bt.grid(row=12,column=1)
+        miao_bt = Button(self.root, text="我要抢秒", command=miao_click_on, bg="green", width=10, fg="blue", bd=5)
+        miao_bt.grid(row=12, column=3)
         self.root.mainloop()
 if __name__=='__main__':
     #apr=reque_num(4000)

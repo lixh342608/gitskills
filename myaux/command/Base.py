f753cc1e49c09c0eca1e4ebea86664075eb91f1f
lixh <278284002@qq.com>
2022-04-16 14:09:05 +0800

    提交

--- myaux/command/Base.py
+++ myaux/command/Base.py
@@ -34,7 +34,7 @@ class myBase():
         self.currtpath=currfiletpath.parent
         self.mous=PyMouse()
         self.tmpfilename = os.path.join(self.currtpath, "temp/tmp.png")
-        self.ditu_size={"jianye":(287,142)}
+        self.ditu_size={"jianye":(287,142),"donghaiwan":(117,117)}
         # 调用百度的识别文字
         APP_ID = '25963522'
         API_KEY = 'RIOFdoDKGXfhHr2uLYVmUG8w'
@@ -74,10 +74,10 @@ class myBase():
         #left, top, right, bottom = self.get_window_rect()
         self.windowleft=left
         self.windowtop = top
-        self.windowright = right-470
-        self.windowbottom = bottom-80
+        self.windowright = right
+        self.windowbottom = bottom
         self.parent_size = (left, top, self.windowright, self.windowbottom)
-        self.dows_center=(self.get_num_center(left,right),self.get_num_center(top,bottom))
+        self.dows_center=(self.get_num_center(left,self.windowright),self.get_num_center(top,self.windowbottom))
     def clear_scene(self):
         self.mous.move(self.dows_center[0],self.dows_center[1])
         time.sleep(1)
@@ -87,7 +87,7 @@ class myBase():
         pyautogui.press("F9")
         pyautogui.hotkey("alt", "h")
         time.sleep(1)
-    def get_pic_center(self,picfile,confidence=0.6):
+    def get_pic_centerforaytogui(self,picfile,confidence=0.6):
         """
         根据基准图片获取操作对象位置
         :param picfile: 基准图片
@@ -99,29 +99,30 @@ class myBase():
         grids=pyautogui.locateCenterOnScreen(picpath,confidence=confidence,region=self.parent_size,grayscale=True)
         return grids
     def dhclick(self,grids,scenal=False):
-        gridx = grids.x-7
-        gridy = grids.y-7
+        gridx = grids[0]
+        gridy = grids[1]
         if scenal:
             gridx=self.get_num_center(gridx,self.dows_center[0])
             gridy = self.get_num_center(gridy, self.dows_center[1])
         #pyautogui.moveTo(gridx,gridy-3)
         print(gridx,gridy-3)
-        self.mous.move(int(gridx),int(gridy-8))
+        self.mous.move(int(gridx)-8,int(gridy-8))
         time.sleep(1)
         pyautogui.click()
         #self.mous.click(int(gridx),int(gridy-8),1,1)
         time.sleep(2)
-    def getpic_click(self,picfile,checkfile=None,check_tag=True,confidence=0.6,trys=10):
+    def getpic_click(self,picfile,checkfile=None,check_tag=True,confidence=0.5,trys=10):
         if checkfile:
             try_num=0
             while try_num <= trys:
                 try:
                     self.clear_scene()
-                    grids=self.get_pic_center(picfile,confidence=confidence)
+                    grids=self.get_pic_centerforaytogui(picfile,confidence=confidence)
                     self.dhclick(grids)
                 except Exception as e:
+                    print("失败了")
                     print(e)
-                if (self.get_pic_center(checkfile) and check_tag) or (not self.get_pic_center(checkfile) and not check_tag):
+                if (self.get_pic_centerforaytogui(checkfile) and check_tag) or (not self.get_pic_centerforaytogui(checkfile) and not check_tag):
                     break
                 else:
                     try_num+=1
@@ -188,7 +189,7 @@ class myBase():
             point=self.get_pointpic_bybaidu()
         return point
 
-    def get_pic_centerforaircv(self, picfile, confidence=0.6):
+    def get_pic_foraircv(self, picfile, confidence=0.6):
         picdir=picfile.split(".")[0].strip("123456789")+"pic"
         picpath=os.path.join(self.currtpath,"pic",picdir,picfile)
         snapshot_pic=self.get_snapshot()
@@ -198,31 +199,35 @@ class myBase():
         if result:
             print(result.get("confidence"))
             rec=result.get('rectangle')
-            #img=Image.open(snapshot_pic)
-            #im=img.crop(rec[0]+rec[-1])
-            #im.save(os.path.join(self.currtpath,"pic.png"))
-            return rec[0]+rec[-1]
+            return self.suoxiaosource(rec[0])+self.suoxiaosource(rec[-1],lv=0.015,is_add=False)
         else:
             return None
     def get_pixel_rate(self,sec,bbox):
         max_grid=self.ditu_size.get(sec)
         x1,y1,x2,y2=bbox
-        xt=(x2-x1-15)/max_grid[0]
-        yt=(y2-y1-15)/max_grid[1]
+        xt=(x2-x1)/max_grid[0]
+        yt=(y2-y1)/max_grid[1]
         return (xt,yt)
+    def get_pic_center(self,picfile,confidence=0.6):
+        bbox=self.get_pic_foraircv(picfile,confidence=0.6)
+        if bbox:
+            return (self.get_num_center(bbox[0],bbox[2]),self.get_num_center(bbox[1],bbox[3]))
+    def suoxiaosource(self,bbox,lv=0.005,is_add=True):
+        if is_add:
+            lv=lv+1
+        else:
+            lv=1-lv
+        return (bbox[0]*lv,bbox[1]*lv)
 
     def positioning(self,scene_name,x,y):
-        if scene_name == "jianye":
-            picname="jianye1.png"
-        else:
-            return None
+        picname=scene_name+".PNG"
         pyautogui.press("tab")
         time.sleep(2)
-        bbox=self.get_pic_centerforaircv(picname)
+        bbox=self.get_pic_foraircv(picname)
         xt,yt=self.get_pixel_rate(scene_name,bbox)
         minix=bbox[0]
         maxy=bbox[-1]
-        grids=(minix+int(x*xt)+3,maxy-int(y*yt)-11)
+        grids=(minix+int(x*xt),maxy-int(y*yt))
         print(bbox)
         print(grids)
         pyautogui.moveTo(grids)
@@ -231,6 +236,46 @@ class myBase():
         #while not self.get_pic_centerforaircv("checkdt2.png",confidence=0.5):
 
         pyautogui.click()
+    def shiyongwupin(self,wupinpic):
+        while not self.get_pic_centerforaytogui(wupinpic,confidence=0.8):
+            pyautogui.hotkey("alt","e")
+        grids=self.get_pic_centerforaytogui(wupinpic,confidence=0.8)
+        pyautogui.moveTo(grids)
+        pyautogui.click(button="right")
+    def yidongfx(self,fx=True,fy=True):
+        dx=self.dows_center[0]
+        dy=self.dows_center[1]
+        print(self.dows_center)
+        px = dx+100 if fx else dx-100
+        py = dy+120 if fy else dy-120
+        print(px,py)
+        pyautogui.moveTo(px,py)
+        pyautogui.click()
+        time.sleep(2)
+    def is_zhantouz(self):
+        if self.get_pic_centerforaytogui("zhandou1.PNG",confidence=0.8):
+            return True
+        return False
+    def zhandoucz(self):
+        while self.is_zhantouz():
+            pyautogui.hotkey("alt", "q")
+            pyautogui.hotkey("alt", "q")
+            pyautogui.hotkey("alt", "a")
+            pyautogui.hotkey("alt", "a")
+            time.sleep(10)
+    def huangdang(self,scene_name):
+        px=random.choice(range(10,100))
+        py=random.choice(range(10,100))
+        self.positioning(scene_name,px,py)
+        for i in range(5):
+            if self.is_zhantouz():
+                self.zhandoucz()
+            else:
+                time.sleep(5)
+
+
+
+
 
 
 
--- myaux/myaux_test.py
+++ myaux/myaux_test.py
@@ -13,8 +13,15 @@ if __name__=="__main__":
     hwnd=Hwnd()
     my_hwnds=hwnd.get_hwnd()
     for key in my_hwnds.keys():
+        print(my_hwnds.get(key))
         hwndbase=myBase(int(key))
+        print(hwndbase.parent_size)
         #hwndbase.get_scene()
-        #hwndbase.getpic_click("wuyi1.PNG","aswuyi.PNG")
-        #hwndbase.getpic_click("wuyidh1.PNG","aswuyi.PNG",check_tag=False,confidence=0.8)
-        hwndbase.positioning("jianye",211,100)
+        #while True:
+            #hwndbase.getpic_click("wuyi1.PNG","aswuyi.PNG")
+            #hwndbase.getpic_click("wuyidh1.PNG","aswuyi.PNG",check_tag=False,confidence=0.8)
+        #hwndbase.positioning("jianye",90,80)
+        #hwndbase.shiyongwupin("baozi1.PNG")
+        for i in range(10):
+            hwndbase.huangdang("donghaiwan")
+            print("jieshu1ci")
